9bb6b2eefa4c77d820e3e686aa45d9ad30a996ee
이창한 <momo0170@naver.com>
2023-09-13 11:09:32 +0900

    apply to tablet responsive web design on the Cart component

---
+++ .history/src/router/Cart_20230913103150.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import CartProduct from '../components/CartProduct';
+import PriceCard from '../components/PriceCard';
+import styles from '../css/Cart.module.css';
+import useCart from '../hooks/useCart';
+import Payment from '../components/Payment';
+import uuid4 from 'uuid4';
+import { useNavigate } from 'react-router-dom';
+
+const SHIPPING = 3000;
+export default function Cart() {
+  const navigate = useNavigate();
+  const goToShopping = () => {
+    navigate('/');
+  };
+  const {
+    cartProduct: { data: cart },
+  } = useCart();
+
+  const hasProduct = cart && cart.length > 0; // 1개 이상 있다면
+  const totalPrice =
+    cart &&
+    cart.reduce((prev, value) => prev + value.price * value.quantity, 0);
+
+  return (
+    cart && (
+      <>
+        <div className={styles.informations}>
+          <div className={styles.list}>
+            <h1>장바구니</h1>
+            <div className={styles.lineFirst}></div>
+            {!hasProduct && (
+              <div className={styles.noProduct}>
+                <p>장바구니에 상품이 없습니다.</p>
+                <button onClick={goToShopping}>쇼핑하러 가기</button>
+              </div>
+            )}
+            {hasProduct &&
+              cart.map((item) => (
+                <li key={uuid4()}>
+                  <CartProduct product={item} />
+                </li>
+              ))}
+            <div className={styles.lineLast}></div>
+          </div>
+
+          {/* 결제 내역 */}
+          <div className={styles.price}>
+            <div className={styles.lineFirst}></div>
+            <h1>결제 내역</h1>
+
+            <PriceCard text="상품 총액" price={totalPrice} />
+            <PriceCard text="배송액" price={SHIPPING} />
+            <div className={styles.lineLast}></div>
+            <PriceCard
+              text="총 가격"
+              price={totalPrice && totalPrice + SHIPPING}
+            />
+            <Payment />
+          </div>
+        </div>
+      </>
+    )
+  );
+}
---
+++ .history/src/router/Edit_20230913101700.jsx
@@ -0,0 +1,134 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import styles from '../css/Edit.module.css';
+import ModalQuestion from '../modal/ModalQuestion';
+import ModalSuccess from '../modal/ModalSuccess';
+
+export default function Edit() {
+  const navigate = useNavigate();
+  const [product, setProduct] = useState({});
+  const [file, setFile] = useState();
+  const [isRegister, setIsRegister] = useState(false);
+  const [isClick, setIsClick] = useState(false);
+
+  const goToHome = () => {
+    navigate('/');
+  };
+  const handleChange = (e) => {
+    setFile(e.target.files);
+    console.log(e);
+  };
+  const handleDataChange = (e) => {
+    setProduct({ ...product, [e.target.name]: e.target.value });
+  };
+
+  // 등록하기 버튼 클릭
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsClick(!isClick);
+  };
+
+  return (
+    <>
+      {
+        <ModalQuestion
+          isClick={isClick}
+          setIsRegister={setIsRegister}
+          setIsClick={setIsClick}
+          file={file}
+          product={product}
+        />
+      }
+      {<ModalSuccess isRegister={isRegister} setIsRegister={setIsRegister} />}
+      <main className={styles.main}>
+        <form onSubmit={handleSubmit} className={styles.form}>
+          <span>제품 등록</span>
+          {/* 이미지 업로드 */}
+          <div className={styles.imgAndUpload}>
+            {/* 이미지 */}
+            <div className={styles.image}>
+              {file && <img src={URL.createObjectURL(file[0])} alt="img" />}
+            </div>
+
+            {/* 파일 박스 */}
+            <div className={styles.fileBox}>
+              <input
+                value={file ? file[0].name : '첨부파일'}
+                className={styles.imageName}
+                disabled
+              />
+              <input
+                id="uploadFile"
+                name="file"
+                type="file"
+                accept="image/*"
+                onChange={handleChange}
+                required
+                className={styles.upload}
+              />
+              <label htmlFor="uploadFile" className={styles.label}>
+                파일찾기
+              </label>
+            </div>
+          </div>
+          {/* 입력란 */}
+          <div className={styles.inputs}>
+            <input
+              name="name"
+              type="text"
+              placeholder="제품명"
+              required
+              onChange={handleDataChange}
+              className={styles.name}
+            />
+            <input
+              name="price"
+              type="text"
+              placeholder="가격"
+              required
+              onChange={handleDataChange}
+              className={styles.price}
+            />
+            <input
+              name="category"
+              type="text"
+              placeholder="카테고리"
+              required
+              onChange={handleDataChange}
+              className={styles.category}
+            />
+            <input
+              name="description"
+              type="text"
+              placeholder="제품 설명"
+              required
+              onChange={handleDataChange}
+              className={styles.description}
+            />
+            <input
+              name="option"
+              type="text"
+              placeholder="옵션"
+              required
+              onChange={handleDataChange}
+              className={styles.option}
+            />
+            {/* 버튼 */}
+            <div className={styles.buttons}>
+              <button type="submit" className={styles.registerBtn}>
+                등록하기
+              </button>
+              <button
+                type="button"
+                onClick={goToHome}
+                className={styles.goToHomeBtn}
+              >
+                메인
+              </button>
+            </div>
+          </div>
+        </form>
+      </main>
+    </>
+  );
+}
